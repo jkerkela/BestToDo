2191ba6b262d6bbfeee68666a2355ac39b7df639
Joni <jonikerkela@hotmail.com>
2018-03-24 18:38:52 +0200

    implement init listfragment

--- app/src/main/java/com/example/j/besttodo/MainActivity.java
+++ app/src/main/java/com/example/j/besttodo/MainActivity.java
@@ -5,21 +5,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
+
 public class MainActivity extends AppCompatActivity {
 
-    ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageButton = findViewById(R.id.addNewTodoItemButton);
+        ImageButton imageButton = findViewById(R.id.addNewTodoItemButton);
 
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO: add new todo item to relative layout view
-
+                //TODO: add new todo item to fragment
             }
         });
     }
---
+++ app/src/main/java/com/example/j/besttodo/TodoListFragment.java
@@ -0,0 +1,37 @@
+package com.example.j.besttodo;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+public class TodoListFragment extends ListFragment implements OnItemClickListener {
+
+    String todoItemsList[];
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.todo_list_fragment, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, todoItemsList);
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,long id) {
+        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
+    }
+}
