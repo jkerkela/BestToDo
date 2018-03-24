17a62f607166bd577aab0ba7b66e213ad9275a4c
Joni <jonikerkela@hotmail.com>
2018-03-24 15:17:34 +0200

    add imagebutton and relativelayout to main view

--- app/src/main/java/com/example/j/besttodo/MainActivity.java
+++ app/src/main/java/com/example/j/besttodo/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.j.besttodo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageButton = findViewById(R.id.addNewTodoItemButton);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: add new todo item to relative layout view
+
+            }
+        });
     }
+
 }
