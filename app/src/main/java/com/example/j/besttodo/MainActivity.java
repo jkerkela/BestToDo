494c48f2c1bba765bbbf5d80e1e953c08dd06f08
Joni <jonikerkela@hotmail.com>
2018-04-01 16:06:19 +0300

    implement adding items to listview dynamically

--- app/src/main/java/com/example/j/besttodo/MainActivity.java
+++ app/src/main/java/com/example/j/besttodo/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.j.besttodo;
 
-import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.ListFragment;
 import android.view.View;
 import android.support.design.widget.FloatingActionButton;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 
+import java.util.List;
 
 public class MainActivity extends FragmentActivity {
 
 
+    TodoListFragment listFragment;
+    FragmentTransaction fragmentTransaction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,15 +24,19 @@ public class MainActivity extends FragmentActivity {
         FloatingActionButton floatingActionButton = findViewById(R.id.addNewTodoItemButton);
 
         if (savedInstanceState == null) {
-            Fragment newFragment = new TodoListFragment();
-            FragmentTransaction ft = getFragmentManager().beginTransaction();
-            ft.add(R.id.listFragment, newFragment).commit();
+            listFragment = new TodoListFragment();
+            fragmentTransaction = getFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.listFragment, listFragment).commit();
         }
 
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO: add new todo item to fragment
+                ArrayAdapter todoListAdapter = (ArrayAdapter) listFragment.getListAdapter();
+                List<String> currentTodoList = listFragment.getTodoList();
+                //TODO: add new todo item with default text to fragment
+                currentTodoList.add("testADD");
+                todoListAdapter.notifyDataSetChanged();
             }
         });
     }
--- app/src/main/java/com/example/j/besttodo/TodoListFragment.java
+++ app/src/main/java/com/example/j/besttodo/TodoListFragment.java
@@ -7,27 +7,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
-import android.widget.Toast;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TodoListFragment extends ListFragment {
 
-    //TODO: initiate list as empty, add items dynamically from button click
-    List<String> todoItemsList = Arrays.asList("test", "test2");
+    List<String> todoItemsList = new ArrayList<>();
+    ArrayAdapter arrayAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        ArrayAdapter adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, todoItemsList);
-        setListAdapter(adapter);
+        arrayAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, todoItemsList);
+        setListAdapter(arrayAdapter);
         return super.onCreateView(inflater, container, savedInstanceState);
 
     }
 
+    public List<String> getTodoList(){
+        return todoItemsList;
+    }
 }
