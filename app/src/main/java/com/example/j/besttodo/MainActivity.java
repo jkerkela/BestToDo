a03f57fe81ecb4417340a52e2ebd4a237406cba2
Joni <jonikerkela@hotmail.com>
2018-04-01 23:24:29 +0300

    changed list item type to edittext, initiated not working default text setting to it

--- app/src/main/java/com/example/j/besttodo/MainActivity.java
+++ app/src/main/java/com/example/j/besttodo/MainActivity.java
@@ -3,11 +3,11 @@ package com.example.j.besttodo;
 import android.app.FragmentTransaction;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
 import android.view.View;
 import android.support.design.widget.FloatingActionButton;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -33,9 +33,11 @@ public class MainActivity extends FragmentActivity {
             @Override
             public void onClick(View v) {
                 ArrayAdapter todoListAdapter = (ArrayAdapter) listFragment.getListAdapter();
-                List<String> currentTodoList = listFragment.getTodoList();
-                //TODO: add new todo item with default text to fragment
-                currentTodoList.add("testADD");
+                List<EditText> currentTodoList = listFragment.getTodoList();
+                EditText editText = new EditText(getApplicationContext());
+                //TODO: this doesnt seem to add text to edittext
+                editText.setText(getResources().getString(R.string.todoItemText), TextView.BufferType.EDITABLE);
+                currentTodoList.add(editText);
                 todoListAdapter.notifyDataSetChanged();
             }
         });
--- app/src/main/java/com/example/j/besttodo/TodoListFragment.java
+++ app/src/main/java/com/example/j/besttodo/TodoListFragment.java
@@ -8,25 +8,26 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TodoListFragment extends ListFragment {
 
-    List<String> todoItemsList = new ArrayList<>();
+    List<EditText> todoItemsList = new ArrayList<>();
     ArrayAdapter arrayAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        arrayAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, todoItemsList);
+        arrayAdapter = new ArrayAdapter<>(getActivity(), R.layout.editable_text_item, todoItemsList);
         setListAdapter(arrayAdapter);
         return super.onCreateView(inflater, container, savedInstanceState);
 
     }
 
-    public List<String> getTodoList(){
+    public List<EditText> getTodoList(){
         return todoItemsList;
     }
 }
