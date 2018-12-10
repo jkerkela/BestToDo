68a427e29d977ae2a102e9b020f90d54ed846385
Joni <jonikerkela@hotmail.com>
2018-12-10 21:54:09 +0200

    implement slide menu skeleton

--- app/src/main/java/com/example/j/besttodo/MainActivity.java
+++ app/src/main/java/com/example/j/besttodo/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.j.besttodo;
 import android.app.FragmentTransaction;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.support.design.widget.FloatingActionButton;
 import android.view.inputmethod.InputMethodManager;
@@ -12,11 +16,27 @@ import android.widget.RelativeLayout;
 public class MainActivity extends AppCompatActivity {
 
     private TodoListFragment mListFragment = new TodoListFragment();
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle toggle;
+    private ActionBar supportActionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO: https://code.tutsplus.com/tutorials/how-to-code-a-navigation-drawer-in-an-android-app--cms-30263
+        Toolbar toolbar = findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        supportActionBar = getSupportActionBar();
+        supportActionBar.setDisplayHomeAsUpEnabled(true);
+        supportActionBar.setHomeButtonEnabled(true);
+
         InitiateBaseFocusHolder();
         InitiateToDoItemAdderButton();
 
